d5b93d9e216cf0d3d5abe3baf9a5485eadbab402
kiwi0fruit <peter.zagubisalo@gmail.com>
2016-06-05 14:13:22 +0600

    Commit 3

--- 7 download extra packages to env cache.cmd
+++ 7 download extra packages to env cache.cmd
@@ -3,6 +3,13 @@ set PATH=%PATH%;%PYTHONPATH%;%PYTHONPATH%\Scripts;%PYTHONPATH%\Library\bin;
 
 set here=%~dp0
 set thispath=%here:~0,-1%
+set funcs=%thispath%\files\functions.cmd
+
+
+call "%funcs%" checkvars
+pause
+
+
 set envcache=%pypath%\%pyfolder%\pkgs\envs\%pyenv%_%pyver%
 mkdir "%envcache%"
 
