fcb22c257ef76ef0710307ebb8a9a21dc58de42d
kiwi0fruit <peter.zagubisalo@gmail.com>
2016-06-07 17:38:48 +0600

    Commit 7

--- __hidden/output_example_without_interpreter/PyQtGraph to Enaml.cmd
+++ __hidden/output_example_without_interpreter/PyQtGraph to Enaml.cmd
@@ -9,5 +9,8 @@ set pyscript=pyqtgraph2enaml\pyqtgraph2enaml.py
 set here=%~dp0
 set thispath=%here:~0,-1%
 call "%thispath%\config\config.cmd"
+IF exist "%thispath%\config\env.cmd" (
+	call "%thispath%\config\env.cmd"
+)
 cd /d %thispath%\%pyfolder%\envs\%pyenv%_%pyver%
 python "%thispath%\%pyscript%"
