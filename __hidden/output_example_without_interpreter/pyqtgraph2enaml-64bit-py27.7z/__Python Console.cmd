b13158b9ac0118b07c2c7be354fd3cc877a81054
kiwi0fruit <peter.zagubisalo@gmail.com>
2016-06-11 23:06:47 +0600

    Commit ?

--- __hidden/output_example_without_interpreter/pyqtgraph2enaml-64bit-py27.7z/__Python Console.cmd
+++
@@ -1,9 +0,0 @@
-set here=%~dp0
-set thispath=%here:~0,-1%
-call "%thispath%\config\config.cmd"
-
-set PYTHONPATH=%thispath%\%pyfolder%
-set PATH=%PYTHONPATH%;%PYTHONPATH%\Scripts;%PYTHONPATH%\Library\bin;%PATH%
-
-cd /d %thispath%\%pyfolder%\Scripts
-cmd /k "activate %pyenv%_%pyver%"
