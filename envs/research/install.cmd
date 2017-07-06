9d4044a2644c32f031c581933503104b250a487e
kiwi0fruit <peter.zagubisalo@gmail.com>
2017-07-06 15:59:41 +0700

    commit

--- envs/research/install.cmd
+++ envs/research/install.cmd
@@ -4,18 +4,14 @@ cmd "/c conda list"
 @REM  R:
 cmd "/c conda install --force -c r rpy2"
 
+@REM  tensorflow:
+cmd "/c conda remove --force markdown"
+
 @REM  =========================================================
 
 @REM  update setuptools:
 cmd "/c conda remove --force setuptools"
 pip install --ignore-installed setuptools
-
-@REM  check pip dependencies:
-cd /d "%envcache%"
-rmdir pip /s /q
-cmd "/c conda list"
-pip download -d "%envcache%\pip" opencv_python knotr tensorflow
-@pause
 
 @REM  post-update:
 @REM  cmd "/c conda install -c conda-forge protobuf=3.3.0 entrypoints=0.2.3 markupsafe=1.0"
--- envs/research/install2.cmd
+++ envs/research/install2.cmd
@@ -1,3 +1,16 @@
+set tensorflow=tensorflow-1.2.1-cp36-cp36m-win_amd64.whl
+
+@REM  check pip dependencies:
+cd /d "%envcache%"
+rmdir pip /s /q
+cmd "/c conda list > conda_list.txt"
+pause
+pip download -d pip opencv_python knotr %tensorflow%
+cd pip
+dir /b /a-d > ..\pip_list.txt
+cd ..
+@pause
+
 @REM  opencv:
 pip install opencv_python
 @pause
@@ -7,4 +20,4 @@ pip install git+%EnvCacheURL%/stitch#egg=knotr
 @REM  pip install git+https://github.com/kiwi0fruit/stitch.git#egg=knotr
 
 @REM  tensorflow - last because of setuptools update:
-pip install tensorflow
+pip install "%envcache%\%tensorflow%"
