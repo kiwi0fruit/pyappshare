45ae68bab6d45f9f564a0b3d3384c7e2d901ee13
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-01-16 23:26:22 +0700

    commit

--- envs/research/install2.cmd
+++ envs/research/install2.cmd
@@ -6,22 +6,21 @@ cd /d "%envcache%"
 
 ::# Conda post-update:
 ::# downgrade pandoc because of it's bug found in 'pipResolve':
-call conda install -c defaults -c conda-forge "pandoc>=2.0,<2.1"
+::call conda install -c defaults -c conda-forge "pandoc>=2.0,<2.1"
 ::# 'pipResolve' found conda/pip versions mismatch: jupyter_client mistune shutilwhich bleach jedi psutil
-call conda install -c defaults -c conda-forge "jupyter_client=5.2.1" "mistune=0.8.3" shutilwhich
-call conda remove --force bleach jedi psutil
-call conda install --copy -c defaults -c conda-forge bleach jedi psutil
+call conda remove --force bleach jedi psutil jupyter_client mistune
+call conda install --copy -c defaults -c conda-forge bleach jedi psutil jupyter_client mistune
 @pause
 
 ::# Check pip dependencies:
-call "%funcs%" pipResolve "pandoctools"
+call "%funcs%" pipResolve "pandoctools knitty sugartex"
 @pause
 
 ::# Tensorflow:
 ::pip install "%envcache%\%tensorflow%"
 
 ::# Pandoctools:
-pip install "git+https://github.com/kiwi0fruit/pandoctools.git"
+pip install pandoctools knitty sugartex
 @pause
 
 call conda install --force -c r rpy2
