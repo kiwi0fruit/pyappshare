520ecfc607a780a3e0f24b93723d4b8c8a846f1d
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-06 17:08:22 +0700

    commit

---
+++ template_app/app-ready/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    """
+    Post-installation for installation mode.
+    """
+    def run(self):
+        # ---------------------------------
+        from shortcutter import ShortCutter
+        from app import test
+
+        sc = ShortCutter(raise_errors=False)
+        sc.create_desktop_shortcut(test())
+
+        # ---------------------------------
+        install.run(self)
+
+
+setup(
+    name='app-ready',
+    version='0.1.0',
+    cmdclass={'install': PostInstallCommand},
+
+    description='My app shortcuts.',
+    url='https://github.com/me/app-ready',
+    author='My Name',
+    author_email='me@mail.com',
+    license='Proprietary',
+
+    install_requires=['app'],
+)
---
+++ template_app/app/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='app',
+    version='0.1.0',
+
+    description='My app.',
+    url='https://github.com/me/app',
+    author='My Name',
+    author_email='me@mail.com',
+    license='Proprietary',
+
+    packages=find_packages(),
+    install_requires=['shortcutter'],
+)
