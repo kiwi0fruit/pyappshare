39e734618be7abfca0e3cd5d388051521c5f0478
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-14 15:12:10 +0700

    commit

--- template_app/app-ready/setup.py
+++ template_app/app-ready/setup.py
@@ -4,27 +4,28 @@ from setuptools.command.install import install
 
 class PostInstallCommand(install):
     def run(self):
-        from shortcutter import ShortCutter
-        from app import test
         import io
-        from os import path as p
         import os
-        import site
+        from os import path as p
+        import app
+        from shortcutter import ShortCutter
+        import inspect
+
         error_log = io.StringIO()
         sc = ShortCutter(error_log=error_log)
-        sc.create_desktop_shortcut(test())
+        sc.create_desktop_shortcut(app.test())
         sc.create_desktop_shortcut('pip')
         sc.create_desktop_shortcut('app')
         sc.create_shortcut_to_env_terminal(menu=False)
 
         sc.activate_args = (None, None)
         sc.create_desktop_shortcut('app', 'app_naked')
-        sc.create_desktop_shortcut(p.join(sc.site_packages, 'app'), 'app_dir')
+        sc.create_desktop_shortcut(p.dirname(inspect.getfile(app)), 'app_dir')
 
         f = open(p.join(p.expanduser('~'), 'app_install_error_log.txt'),
                  'w', encoding="utf-8")
         print(error_log.getvalue(), file=f)
-        print(site.getsitepackages(), '\n', sc.site_packages, '\n', sc.bin_folder, file=f)
+        print(sc.bin_folder, file=f)
 
         f.close()
         error_log.close()
