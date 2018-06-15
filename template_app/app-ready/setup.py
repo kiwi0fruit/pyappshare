b2240eef912dbc1f0d2991e1f4fa7d70403e5625
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-16 01:15:59 +0700

    Update setup.py

--- template_app/app-ready/setup.py
+++ template_app/app-ready/setup.py
@@ -12,23 +12,16 @@ class PostInstallCommand(install):
         import inspect
 
         error_log = io.StringIO()
+
         sc = ShortCutter(error_log=error_log)
-        sc.create_desktop_shortcut(app.test())
-        sc.create_desktop_shortcut('pip')
         sc.create_desktop_shortcut('app')
+        sc.create_desktop_shortcut(p.dirname(inspect.getfile(app)), 'app_dir')
         sc.create_shortcut_to_env_terminal(menu=False)
 
-        sc.activate_args = (None, None)
-        sc.create_desktop_shortcut('app', 'app_naked')
-        sc.create_desktop_shortcut(p.dirname(inspect.getfile(app)), 'app_dir')
-
-        f = open(p.join(p.expanduser('~'), 'app_install_error_log.txt'),
-                 'w', encoding="utf-8")
-        print(error_log.getvalue(), file=f)
-        # print(sc.bin_folder, file=f)
-
-        f.close()
+        print(error_log.getvalue(), file=open(p.join(p.expanduser('~'), 'app_install_error_log.txt'),
+                                              'w', encoding="utf-8"))
         error_log.close()
+
         # must have:
         install.run(self)
 
