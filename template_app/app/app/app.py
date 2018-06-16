0dcc29cf4a2dfe9ce66d1dc15e8ab746f658c24e
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-17 00:29:17 +0700

    commit

---
+++ template_app/app/app/app.py
@@ -0,0 +1,20 @@
+# prevent GPL infecting:
+import pip
+
+class LicenseError(Exception):
+    pass
+
+blacklisted_pkgs = ['PyQt4', 'PyQt5', 'sip', 'PyQt', 'pyqt']
+installed_pkgs = [pkg.key for pkg in pip.get_installed_distributions()]
+for package in blacklisted_pkgs:
+    if package in installed_pkgs:
+        raise LicenseError('Uninstall {} module to use the app.'.format(package))
+#
+import os
+
+def test():
+    return 'python'
+
+def cli():
+    print(os.environ.get('PATH'))
+    input("Press Enter to exit.")
