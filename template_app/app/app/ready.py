2dc026241641a31b0b888ba1ee73837f23291de1
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-03-06 16:29:52 +0700

    bugfix

--- template_app/app/app/ready.py
+++ template_app/app/app/ready.py
@@ -1,8 +1,5 @@
 import sys
-import app  # make sure the main module is installed
-import io
 from os import path as p
-import inspect
 from shortcutter import ShortCutter
 
 
@@ -11,12 +8,15 @@ def ready():
     Creates desktop shortcuts to app exec. and app module folder
     via shortcutter: https://github.com/kiwi0fruit/shortcutter
     """
+    # Find module:
+    #   import the_module
+    #   import inspect
+    #   mod_dir = p.dirname(inspect.getfile(the_module))
     # Find module without importing:
-    # from importlib.util import find_spec as find_module
-    # find_module('app').origin
+    #   from importlib.util import find_spec as find_module
+    #   find_module('the_module').origin
 
     sc = ShortCutter(error_log=sys.stderr)
     sc.create_desktop_shortcut('app')
-    app_dir = p.dirname(inspect.getfile(app))
-    sc.create_desktop_shortcut(p.join(app_dir, 'extra'), 'app_extra_dir')
+    sc.create_desktop_shortcut(p.join(p.dirname(__file__), 'extra'), 'app_extra_dir')
     sc.create_shortcut_to_env_terminal(menu=False)
